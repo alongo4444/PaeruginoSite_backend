bce7bbdde3efab1da952180033b519c179673577
efronio <idoefroni@gmail.com>
2021-01-02 15:12:27 +0200

    ido commit

--- app/main.py
+++ app/main.py
@@ -53,4 +53,4 @@ app.include_router(auth_router, prefix="/api", tags=["auth"])
 
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="127.0.0.1", reload=True, port=8800)
+    uvicorn.run("main:app", host="0.0.0.0", reload=True, port=8800)
