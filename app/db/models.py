51b079914415f7056bc1e873fea7860a13b7076b
efronio <idoefroni@gmail.com>
2021-01-04 11:30:07 +0200

    ido commit strains

--- app/db/models.py
+++ app/db/models.py
@@ -3,18 +3,6 @@ from sqlalchemy import Boolean, Column, Integer, String, Text,Numeric
 from .session import Base
 
 
-class User(Base):
-    __tablename__ = "user"
-
-    id = Column(Integer, primary_key=True, index=True)
-    email = Column(String, unique=True, index=True, nullable=False)
-    first_name = Column(String)
-    last_name = Column(String)
-    hashed_password = Column(String, nullable=False)
-    is_active = Column(Boolean, default=True)
-    is_superuser = Column(Boolean, default=False)
-
-
 class Genes(Base):
     __tablename__ = "Genes"
 
@@ -33,3 +21,9 @@ class Genes(Base):
     product_length_y = Column("product_length_y", Numeric)
     dna_sequence = Column("dna_sequence", Text)
     protein_sequence = Column("protein_sequence", Text)
+
+class Genes(Base):
+    __tablename__ = "Strains"
+
+    Assembly = Column("Assembly", Text, primary_key=True, index=True, nullable=False)
+    Strain = Column("Strain", Text)
