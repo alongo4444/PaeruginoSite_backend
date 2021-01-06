b7b3088d29e4d802faff5c74f1aa1e4b99a3b0d4
yinonbz <yinonbz@gmail.com>
2021-01-06 15:10:52 +0200

    phylogenetic tree

--- app/api/api_v1/routers/strains.py
+++ app/api/api_v1/routers/strains.py
@@ -1,9 +1,13 @@
 from fastapi import APIRouter, Request, Depends, Response, encoders
-import typing as t
+import subprocess, os
+from fastapi.responses import FileResponse
 from app.db.session import get_db
 from app.db.crud import (
     get_strains,
 )
+
+from PIL import Image
+
 from app.db.schemas import StrainBase
 
 strains_router = r = APIRouter()
@@ -11,7 +15,7 @@ strains_router = r = APIRouter()
 
 @r.get(
     "/strains",
-    #response_model=t.List[StrainBase],
+    # response_model=t.List[StrainBase],
     response_model_exclude_none=True,
 )
 async def strains_list(
@@ -23,3 +27,39 @@ async def strains_list(
     # This is necessary for react-admin to work
     # response.headers["Content-Range"] = f"0-9/{len(users)}"
     return strains
+
+
+@r.get(
+    "/strains/phyloTree",
+    response_model_exclude_none=True,
+)
+async def strains_list(
+        response: Response
+):
+    """Get all strains"""
+
+    r_script = open("static/phylo_tree.R", "w")
+    r_script.write(
+        'library(ggtreeExtra)\n##library(ggstar)\nlibrary(ggplot2)\nlibrary(ggtree)\nlibrary(treeio)\nlibrary(ggnewscale)\nlibrary(ape)\n' +
+        'trfile <- system.file("extdata","tree.nwk", package="ggtreeExtra")\ntree <- read.tree(trfile)\n'+
+        'p <- ggtree(tree, layout="fan", open.angle = 10, size = 0.5)\npng("'
+        + (os.path.abspath("static/phylo_tree.png")).replace('\\', '/') +
+        '", units="cm", width=800, height=800, res=60)\nplot(p + geom_tiplab())\ndev.off(0)')
+    r_script.close()
+    command = 'C:/Program Files/R/R-4.0.3/bin/Rscript.exe'
+    # todo replace with command = 'Rscript'  # OR WITH bin FOLDER IN PATH ENV VAR
+    arg = '--vanilla'
+    try:
+        p = subprocess.Popen([command, arg, os.path.abspath("static/phylo_tree.R")],
+                             cwd=os.path.normpath(os.getcwd() + os.sep + os.pardir), stdin=subprocess.PIPE,
+                             stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+        output, error = p.communicate()
+        return FileResponse('static/phylo_tree.png')
+
+    except Exception as e:
+        print("dbc2csv - Error converting file: phylo_tree.R")
+        print(e)
+
+        return False
+    return False
