eab9069d899a2e9869f4da896c66d982bf3c25a1
tomerBC <tomer@betterchains.com>
2021-03-20 11:54:09 +0200

    changed the file in 4.4

--- app/api/api_v1/routers/strains.py
+++ app/api/api_v1/routers/strains.py
@@ -7,6 +7,9 @@ from app.db.crud import (
     get_strains,
 )
 from pathlib import Path
+from bs4 import BeautifulSoup
+from bs4.element import Tag
+
 from PIL import Image
 
 from app.db.schemas import StrainBase
@@ -72,11 +75,17 @@ async def strains_list(
 )
 async def strain_circos_graph(strain_name, response: Response):
     # the structure of the dir file will be stain_name.html and it will be stored in a specific directory.
-    strain_file = Path("static/"+strain_name+".html")
-    print(strain_file)
+    strain_file = Path("static/"+strain_name+".txt")
     if strain_file.is_file():
+        '''
+        f = open(strain_file, "r", encoding='utf-8')
+        text = f.read()
+        stopwords = ['<!DOCTYPE html>', '<html>', '</html>']
+        for word in stopwords:
+            if word in text:
+                text = text.replace(word, "")
+        '''
         return FileResponse(strain_file, status_code=200)
     else:
         # file is not in the directory (the strain name is wrong)
         return Response(status_code=400)
-
