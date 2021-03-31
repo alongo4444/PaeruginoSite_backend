6aae8c8d2a8539751ae1a40ed720ca668dc93c68
IdoEfroni <idoefroni@gmail.com>
2021-03-31 20:14:46 +0300

    ido commit cluster.py

--- app/db/models.py
+++ app/db/models.py
@@ -16,21 +16,20 @@ class User(Base):
 
 class Genes(Base):
     __tablename__ = "Genes"
-
+    #SELECT index, assembly, genomic_accession, start_g, end_g, strand, symbol, locus_tag,
+    # attributes_x, product_accession, nonredundant_refseq, name, protein_sequence, dna_sequence
+    #
     locus_tag = Column("locus_tag", Text, primary_key=True, index=True, nullable=False)
-    assembly_x =  Column("assembly_x", Text)
+    assembly = Column("assembly", Text)
     attributes_x = Column("attributes_x", Text)
-    chromosome_y = Column("chromosome_y", Text)
-    genomic_accession_y = Column("genomic_accession_y", Text)
-    start_y = Column("start_y", Integer)
-    end_y = Column("end_y", Integer)
-    strand_y = Column("strand_y", String)
-    product_accession_y = Column("product_accession_y", Text)
+    genomic_accession = Column("genomic_accession", Text)
+    start = Column("start_g", Integer)
+    end = Column("end_g", Integer)
+    strand = Column("strand", String)
+    product_accession = Column("product_accession", Text)
     # non-redundant_refseq_y = Column("non-redundant_refseq_y", Text)
-    name_y = Column("name_y", Text)
-    symbol_y = Column("symbol_y", Text)
-    geneID_y = Column("geneID_y", Integer)
-    product_length_y = Column("product_length_y", Numeric)
+    name = Column("name", Text)
+    symbol = Column("symbol", Text)
     dna_sequence = Column("dna_sequence", Text)
     protein_sequence = Column("protein_sequence", Text)
 
